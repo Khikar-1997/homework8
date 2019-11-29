6c25a89b6ebf8da4fee9219e21b994be23f74af8
Khikar-1997 <57430976+Khikar-1997@users.noreply.github.com>
2019-11-29 15:34:31 -0800

    8_4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,24 +5,24 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<String> movies = new ArrayList<>();
-        movies.add("Gotti");
-        movies.add("Titanic");
-        movies.add("The Revenant");
-        movies.add("The Hobbit");
-        movies.add("Braveheart");
+        ArrayList<String> dogBreeds = new ArrayList<>();
+        dogBreeds.add("Rottweiler");
+        dogBreeds.add("Tibetan mastiff");
+        dogBreeds.add("Engelse mastiff");
+        dogBreeds.add("Chau chau");
+        dogBreeds.add("German shepherd");
         System.out.println("This is the first array");
         System.out.print("[ ");
-        for (int i = 0; i < movies.size(); i++) {
-            System.out.print(movies.get(i) + " , ");
+        for (int i = 0; i < dogBreeds.size(); i++) {
+            System.out.print(dogBreeds.get(i) + " , ");
         }
         System.out.print("]");
         System.out.println();
-        movies.remove(2);
+        dogBreeds.remove(2);
         System.out.println("And this is an array with an extracted element ");
         System.out.print("[ ");
-        for (int i = 0; i < movies.size(); i++) {
-            System.out.print(movies.get(i) + " , ");
+        for (int i = 0; i < dogBreeds.size(); i++) {
+            System.out.print(dogBreeds.get(i) + " , ");
         }
         System.out.print("]");
     }
