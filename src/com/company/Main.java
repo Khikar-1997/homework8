09e1a084fba99f6e1357cb153158d6d4d8556fd0
Khikar-1997 <57430976+Khikar-1997@users.noreply.github.com>
2019-11-29 15:33:22 -0800

    8_3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,24 +5,24 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<String> names = new ArrayList<>();
-        names.add("Petros");
-        names.add("Poxos");
-        names.add("Martiros");
-        names.add("Anna");
-        names.add("Mane");
+        ArrayList<String> movies = new ArrayList<>();
+        movies.add("Gotti");
+        movies.add("Titanic");
+        movies.add("The Revenant");
+        movies.add("The Hobbit");
+        movies.add("Braveheart");
         System.out.println("This is the first array");
         System.out.print("[ ");
-        for (int i = 0; i < names.size(); i++) {
-            System.out.print(names.get(i) + " , ");
+        for (int i = 0; i < movies.size(); i++) {
+            System.out.print(movies.get(i) + " , ");
         }
         System.out.print("]");
         System.out.println();
-        System.out.println("And this is an array with a new element in the first position");
-        names.set(0, "Maria");
+        movies.remove(2);
+        System.out.println("And this is an array with an extracted element ");
         System.out.print("[ ");
-        for (int i = 0; i < names.size(); i++) {
-            System.out.print(names.get(i) + " , ");
+        for (int i = 0; i < movies.size(); i++) {
+            System.out.print(movies.get(i) + " , ");
         }
         System.out.print("]");
     }
