19dd3fe78ccaa84fa61c5664bcf90c2d52a18079
Khikar-1997 <57430976+Khikar-1997@users.noreply.github.com>
2019-11-29 15:31:45 -0800

    8_2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,14 +5,25 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<String> colors = new ArrayList<>();
-        colors.add("green");
-        colors.add("red");
-        colors.add("black");
-        colors.add("yellow");
-        colors.add("white");
-        for (int i = 0; i < colors.size(); i++) {
-            System.out.println(colors.get(i));
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Petros");
+        names.add("Poxos");
+        names.add("Martiros");
+        names.add("Anna");
+        names.add("Mane");
+        System.out.println("This is the first array");
+        System.out.print("[ ");
+        for (int i = 0; i < names.size(); i++) {
+            System.out.print(names.get(i) + " , ");
         }
+        System.out.print("]");
+        System.out.println();
+        System.out.println("And this is an array with a new element in the first position");
+        names.set(0, "Maria");
+        System.out.print("[ ");
+        for (int i = 0; i < names.size(); i++) {
+            System.out.print(names.get(i) + " , ");
+        }
+        System.out.print("]");
     }
 }
