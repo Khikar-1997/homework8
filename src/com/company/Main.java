1e8f227aeba39e5864674ef5e6d742530feaaeea
Khikar-1997 <57430976+Khikar-1997@users.noreply.github.com>
2019-11-29 16:18:18 -0800

    8_9

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,24 +5,25 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<String> continents = new ArrayList<>();
-        continents.add("North America");
-        continents.add("Europe");
-        continents.add("Asia");
-        continents.add("Africa");
-        continents.add("Sout America");
-        continents.add("Antarctica");
-        continents.add("Australia");
+        ArrayList<Integer> numbers = new ArrayList<>();
+        int[] arraynumbers = new int[10];
         System.out.println("This is the first array");
         System.out.print("[ ");
-        for (String continent : continents) {
-            System.out.print(continent + " , ");
+        for (int i = 0; i < arraynumbers.length; i++) {
+            arraynumbers[i] = i;
+            numbers.add(arraynumbers[i]);
+            System.out.print(numbers.get(i) + " , ");
+        }
+        System.out.println("]");
+
+        System.out.println();
+        System.out.println("And this is an array with odd numbers");
+        System.out.print("[ ");
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i) % 2 != 0) {
+                System.out.print(numbers.get(i) + " , ");
+            }
         }
         System.out.print("]");
-        System.out.println();
-
-        System.out.println("And this is an empty array");
-        continents.clear();
-        System.out.println(continents);
     }
 }
